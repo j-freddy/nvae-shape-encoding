de87900fc43ef8423d75cfca2ad3a9a87bc0674b
F Jiang <jiang.tf.2020@gmail.com>
2024-04-17 21:22:03 +0100

    Write latent dim and beta tuning script with a smaller test grid

---
+++ scripts/slurm-vae-tune.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=ffj20 # required to send email notifcations - please replace <your_username> with your college login name or email address
+
+. /vol/cuda/12.2.0/setup.sh
+export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.10/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
+TERM=vt100                # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+cd nvae-shape-encoding
+export PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/bin/:$PATH
+source activate
+
+# ==============================================================================
+# [VAE Tune]
+# VAE ACDC: Grid search on beta (KL) and latent dim hyperparameters
+#
+# Time estimate: 7-8 min per run => 8 hrs for 78 runs
+# ==============================================================================
+
+# for latent_dim in 2 4 8 16 32 64
+for latent_dim in 2 4
+do
+    # for beta in 0.01 0.02 0.05 0.1 0.2 0.5 1 2 5 10 20 50 100
+    for beta in 0.01 100
+    do
+        model_name="ld-${latent_dim}-beta-${beta}"
+        # Train
+        python -m arch.vae.train --epochs 50 --latent_dim $latent_dim --beta $beta --model_name $model_name
+        # Get saved model path
+        model_path=$(ls logs/vae_acdc/${model_name}/checkpoints/*.ckpt)
+        # Test: Save figures and metrics
+        python -m arch.vae.test --model_path $model_path --save_fig 
+    done
+done
