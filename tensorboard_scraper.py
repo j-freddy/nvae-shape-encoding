5f8f1c4a1809f6b8d189cda05e43f5bf07afc48a
Freddy Jiang <jtf.imperial@gmail.com>
2024-05-06 18:32:10 +0100

    Write a scraper for TensorBoard as the interface doesn't allow downloading multiple datapoints

---
+++ tensorboard_scraper.py
@@ -0,0 +1,64 @@
+import csv
+import os
+import pandas as pd
+import requests
+
+from const import ACDC, LOGS_PATH
+
+def get_tensorboard_data(log_dir: str, metrics: list[str]) -> pd.DataFrame:
+    """
+    Scrape single-value metrics from TensorBoard logs. Each metric takes ~2
+    seconds. TensorBoard must be active on localhost:6006.
+
+    Example: 2 metrics and 144 experiments = 4.8 minutes
+    """
+    experiments = os.listdir(log_dir)
+
+    df = pd.DataFrame(columns=["model_name"] + metrics)
+    df.set_index("model_name", inplace=True)
+
+    for i, model_name in enumerate(experiments):
+        value_buffer = []
+
+        corrupted = False
+
+        for metric in metrics:
+            url = f"http://localhost:6006/data/plugin/scalars/scalars?tag={metric}&run={model_name}&format=csv"
+
+            response = requests.get(url, allow_redirects=True)
+            data_csv = csv.reader(response.text.splitlines(), delimiter=",")
+
+            header = next(data_csv)
+            
+            if header[-1] != "Value":
+                print(f"Corrupted data. Printing received data for {model_name}")
+                print(header)
+                print(list(data_csv))
+                corrupted = True
+                break
+            
+            value = float(next(data_csv)[-1])
+            value_buffer.append(value)
+
+        if not corrupted:
+            df.loc[model_name] = value_buffer
+
+        print(f"Current progress: {i + 1}/{len(experiments)}")
+    
+    return df
+
+if __name__ == '__main__':
+    # Configuration
+    # TODO Move to argparse
+    log_subdir = "info-adversarial-vae"
+    metrics = ["fid", "test_recon_loss"]
+
+    df = get_tensorboard_data(
+        log_dir=os.path.join(LOGS_PATH, ACDC.DIR.VAE, log_subdir),
+        metrics=metrics,
+    )
+
+    print(df.head())
+
+    # Save dataframe to a csv file
+    df.to_csv(os.path.join(LOGS_PATH, ACDC.DIR.VAE, f"{log_subdir}.csv"))
