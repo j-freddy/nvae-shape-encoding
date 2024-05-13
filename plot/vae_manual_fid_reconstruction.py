ee4458259c7cfd0af0d54be9f00dadaa2d56a5e5
F Jiang <jiang.tf.2020@gmail.com>
2024-05-13 13:22:08 +0100

    Ablation study: effect of registration

--- plot/vae_manual_fid_reconstruction.py
+++ plot/vae_manual_fid_reconstruction.py
@@ -8,18 +8,17 @@ if __name__ == '__main__':
     plt.style.use("ggplot")
 
     paths = [
-        os.path.join(LOGS_PATH, ACDC.DIR.VAE, "beta-vae.csv"),
         os.path.join(LOGS_PATH, ACDC.DIR.VAE, "info-vae.csv"),
-        os.path.join(LOGS_PATH, ACDC.DIR.VAE, "info-adversarial-vae.csv"),
+        os.path.join(LOGS_PATH, ACDC.DIR.VAE, "info-vae-register.csv"),
     ]
 
-    labels = ["beta-VAE", "InfoVAE", "InfoAdversarialVAE"]
+    labels = ["InfoVAE", "InfoVAE-Register-Alignment"]
 
     for label, path in zip(labels, paths):
         df = pd.read_csv(path, index_col="model_name")
 
-        # Filter all rows with fid >= 50
-        df = df[df["fid_manual"] < 50]
+        # Filter all rows with fid >= 25
+        df = df[df["fid_manual"] < 25]
 
         plt.scatter(df["fid_manual"], df["test_recon_loss"], alpha=0.5, label=label)
     
