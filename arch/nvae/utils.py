7473305ebf7fc9ec66676e63edd647dfa87b8fd2
F Jiang <jiang.tf.2020@gmail.com>
2024-07-14 13:45:59 +0100

    Try five latent layers architecture

--- arch/nvae/utils.py
+++ arch/nvae/utils.py
@@ -8,5 +8,10 @@ ID_TO_ARCH = {
         "num_groups_per_layer": [4, 2, 2, 1, 1],
         "is_layer_shared": [True, False, True, False, True],
         "initial_downsample_factor": 2,
-    }
+    },
+    "five-latent": {
+        "num_groups_per_layer": [4, 2, 2, 1, 1],
+        "is_layer_shared": [True, True, True, True, True],
+        "initial_downsample_factor": 2,
+    },
 }
