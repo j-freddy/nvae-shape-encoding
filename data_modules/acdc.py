437322b55c80db113ea761a2677fb4fff5398318
F Jiang <jiang.tf.2020@gmail.com>
2024-07-17 22:30:08 +0100

    Preview ACDC scans and masks

--- data_modules/acdc.py
+++ data_modules/acdc.py
@@ -121,10 +121,10 @@ def get_dataset(test=False) -> tio.SubjectsDataset:
     subjects = []
     
     # noqa
-    # seq = range(101, 151) if test else range(1, 101)
+    seq = range(101, 151) if test else range(1, 101)
     
     # Small subset to speed up preprocessing
-    seq = range(101, 106) if test else range(1, 6)
+    # seq = range(101, 106) if test else range(1, 6)
     
     for i in seq:
         patient_id = str(i).zfill(3)
@@ -364,11 +364,14 @@ class ACDCDataModule(LightningDataModule):
         
         return data_train, data_val
     
-    def train_dataloader(self):
-        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True)
+    def train_dataloader(self, shuffle=True):
+        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=shuffle)
 
-    def test_dataloader(self):
-        return DataLoader(self.data_test, batch_size=self.batch_size, shuffle=False)
+    def val_dataloader(self, shuffle=False):
+        return DataLoader(self.data_val, batch_size=self.batch_size, shuffle=shuffle)
+    
+    def test_dataloader(self, shuffle=False):
+        return DataLoader(self.data_test, batch_size=self.batch_size, shuffle=shuffle)
 
 class ACDCMaskDataModule(LightningDataModule):
     """
