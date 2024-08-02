ad6ea01813c78ab645d6f55b029cd1b193cb0e21
F Jiang <jiang.tf.2020@gmail.com>
2024-08-02 20:04:43 +0100

    Train shape prior with alpha=1

--- scripts/unet/slurm-unet-train-shape-prior.sh
+++ scripts/unet/slurm-unet-train-shape-prior.sh
@@ -15,7 +15,7 @@ source activate
 
 seeds=("1969 1970 1971 1972 1973")
 
-logdir="logs-unet-temp"
+logdir="logs-unet-alpha-1"
 
 # Train
 
@@ -28,7 +28,7 @@ do
         --augment \
         --seed $seed \
         --model_name $model_name \
-        --alpha 0 \
+        --alpha 1 \
         --logs $logdir
 done
 
