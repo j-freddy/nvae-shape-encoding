48d93d394c9754382a9f908c1661949702fb530d
F Jiang <jiang.tf.2020@gmail.com>
2024-08-11 22:28:58 +0100

    Train each model 5 times

--- scripts/slurm/unet/mnms-train-baseline.sh
+++ scripts/slurm/unet/mnms-train-baseline.sh
@@ -20,56 +20,64 @@ source activate
 
 logdir="logs-unet-mnms-baseline"
 
-# Train on full dataset
+# # Train on full dataset
 
-model_name="all-data"
-python -m arch.unet.train \
-    --epochs 100 \
-    --loss_reg "cross_entropy" \
-    --dataset mnms \
-    --augment \
-    --model_name $model_name \
-    --logs $logdir
+# model_name="all-data"
+# python -m arch.unet.train \
+#     --epochs 100 \
+#     --loss_reg "cross_entropy" \
+#     --dataset mnms \
+#     --augment \
+#     --model_name $model_name \
+#     --logs $logdir
 
-# Evaluate on full dataset
+# # Evaluate on full dataset
 
-model_name="all-data"
-# Get saved model path
-model_path=$(ls ${logdir}/unet_mnms/${model_name}/checkpoints/*.ckpt)
-# Test: Save figures and metrics
-python -m arch.unet.test --model_path $model_path --dataset mnms --logs $logdir
+# model_name="all-data"
+# # Get saved model path
+# model_path=$(ls ${logdir}/unet_mnms/${model_name}/checkpoints/*.ckpt)
+# # Test: Save figures and metrics
+# python -m arch.unet.test --model_path $model_path --dataset mnms --logs $logdir
 
 # Train: Few-shot learning
 
 centres=("1 2 3 4 5")
+seeds=("1970 1971 1972 1973 1974")
 
 for centre in $centres
 do
-    model_name="centre-${centre}-5-subjects"
-    python -m arch.unet.train \
-        --epochs 100 \
-        --loss_reg "cross_entropy" \
-        --dataset mnms \
-        --centre $centre \
-        --num_subjects 5 \
-        --sort_by_validity \
-        --augment \
-        --model_name $model_name \
-        --logs $logdir
+    for seed in $seeds
+    do
+        model_name="centre-${centre}-5-subjects-seed-${seed}"
+        python -m arch.unet.train \
+            --epochs 100 \
+            --loss_reg "cross_entropy" \
+            --dataset mnms \
+            --centre $centre \
+            --num_subjects 5 \
+            --sort_by_validity \
+            --augment \
+            --seed $seed \
+            --model_name $model_name \
+            --logs $logdir
+    done
 done
 
 # Evaluate: Few-shot learning
 
 for centre in $centres
 do
-    model_name="centre-${centre}-5-subjects"
-    # Get saved model path
-    model_path=$(ls ${logdir}/unet_mnms/${model_name}/checkpoints/*.ckpt)
-    # Test: Save figures and metrics
-    python -m arch.unet.test \
-        --model_path $model_path \
-        --dataset mnms \
-        --centre $centre \
-        --logs $logdir
+    for seed in $seeds
+    do
+        model_name="centre-${centre}-5-subjects-seed-${seed}"
+        # Get saved model path
+        model_path=$(ls ${logdir}/unet_mnms/${model_name}/checkpoints/*.ckpt)
+        # Test: Save figures and metrics
+        python -m arch.unet.test \
+            --model_path $model_path \
+            --dataset mnms \
+            --centre $centre \
+            --logs $logdir
+    done
 done
 
