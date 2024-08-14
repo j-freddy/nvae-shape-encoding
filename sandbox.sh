249f11dd2dd2506a61499c2b61be1a2a830a7b2c
F Jiang <jiang.tf.2020@gmail.com>
2024-08-14 13:53:59 +0100

    Check consistency of NVAE best model logs

---
+++ sandbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=ffj20 # required to send email notifcations - please replace <your_username> with your college login name or email address
+
+. /vol/cuda/12.2.0/setup.sh
+export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.10/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
+TERM=vt100                # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+cd nvae-shape-encoding
+export PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/bin/:$PATH
+source activate
+
+python -m arch.nvae.test --model_path "logs/nvae_acdc/best/default/checkpoints/epoch=97-step=20972.ckpt"
