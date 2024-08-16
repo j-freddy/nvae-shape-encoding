e56fdc08600517f5ffbc342a51c7977c5f798b3d
F Jiang <jiang.tf.2020@gmail.com>
2024-08-16 17:52:16 +0100

    Add SLURM boilerplate to the script

--- scripts/slurm/unet/mnms-finetune.sh
+++ scripts/slurm/unet/mnms-finetune.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=ffj20 # required to send email notifcations - please replace <your_username> with your college login name or email address
+
+. /vol/cuda/12.2.0/setup.sh
+export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.10/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
+TERM=vt100                # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+cd nvae-shape-encoding
+export PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/bin/:$PATH
+source activate
+
 centres=("1 2 3 4 5")
 seeds=("1970" "1971" "1972" "1973" "1974")
 
