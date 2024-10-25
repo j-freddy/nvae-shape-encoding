6ba90bc589d8da0aa0ce297eb4465fe503ae9dc7
F Jiang <jiang.tf.2020@gmail.com>
2024-10-25 23:08:10 +0100

    Update Python 3.10 to Python 3.12 on SLURM

--- scripts/slurm/nvae_seg/default-tune.sh
+++ scripts/slurm/nvae_seg/default-tune.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=ffj20 # required to send email notifcations - please replace <your_username> with your college login name or email address
 
 . /vol/cuda/12.2.0/setup.sh
-export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.10/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.12/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
 TERM=vt100                # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
