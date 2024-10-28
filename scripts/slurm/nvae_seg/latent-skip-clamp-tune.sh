71486ba89bbf7efd3f960063e6e9884400c4dc5a
F Jiang <jiang.tf.2020@gmail.com>
2024-10-28 17:39:46 +0000

    Comment out evaluation script for latent skip

--- scripts/slurm/nvae_seg/latent-skip-clamp-tune.sh
+++ scripts/slurm/nvae_seg/latent-skip-clamp-tune.sh
@@ -24,27 +24,27 @@ logdir="logs-nvaeseg-latent-skip-clamp"
 
 # Train
 
-for projected_channels in $projected_channels_list
-do
-    for warmup_steps in $warmup_steps_list
-    do
-        for beta in $betas
-        do
-            model_name="pc-${projected_channels}-ws-${warmup_steps}-b-${beta}"
-            betas_str="${beta},${beta},${beta}"
-            # Train
-            python -m arch.nvaeseg.train \
-                --epochs 100 \
-                --arch "latent-skip" \
-                --projected_channels $projected_channels \
-                --min_channels 16 \
-                --warmup_steps $warmup_steps \
-                --betas $betas_str \
-                --model_name $model_name \
-                --logs $logdir
-        done
-    done
-done
+# for projected_channels in $projected_channels_list
+# do
+#     for warmup_steps in $warmup_steps_list
+#     do
+#         for beta in $betas
+#         do
+#             model_name="pc-${projected_channels}-ws-${warmup_steps}-b-${beta}"
+#             betas_str="${beta},${beta},${beta}"
+#             # Train
+#             python -m arch.nvaeseg.train \
+#                 --epochs 100 \
+#                 --arch "latent-skip" \
+#                 --projected_channels $projected_channels \
+#                 --min_channels 16 \
+#                 --warmup_steps $warmup_steps \
+#                 --betas $betas_str \
+#                 --model_name $model_name \
+#                 --logs $logdir
+#         done
+#     done
+# done
 
 # Evaluate
 
@@ -56,7 +56,7 @@ do
         do
             model_name="pc-${projected_channels}-ws-${warmup_steps}-b-${beta}"
             # Get saved model path
-            model_path=$(ls ${logdir}/nvae_acdc/${model_name}/checkpoints/*.ckpt)
+            model_path=$(ls ${logdir}/nvae_seg_acdc/${model_name}/checkpoints/*.ckpt)
             # Test: Save figures and metrics
             python -m arch.nvaeseg.test --model_path $model_path --logs $logdir
         done
