717bc381a83754d8064817b0cd431c38344c2e06
F Jiang <jiang.tf.2020@gmail.com>
2025-01-26 16:41:34 +0000

    Write script to tune CNVAE

--- scripts/slurm/cnvae/default-train.sh
+++ scripts/slurm/cnvae/default-train.sh
@@ -2,14 +2,17 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=ffj20 # required to send email notifcations - please replace <your_username> with your college login name or email address
+
 . /vol/cuda/12.2.0/setup.sh
 export LD_LIBRARY_PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/lib/python3.12/site-packages/nvidia/cublas/lib:${LD_LIBRARY_PATH}
 TERM=vt100                # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
+
 cd nvae-shape-encoding
 export PATH=/vol/bitbucket/${USER}/nvae-shape-encoding/venv/bin/:$PATH
 source activate
+
 projected_channels_list=("4")
 warmup_steps_list=("6420")
 seeds=("1969 1970 1971 1972 1973 1974 1975 1976 1977 1978")
