19024acbb8888631b9462f5cbfe6c36158eb4edc
F Jiang <jiang.tf.2020@gmail.com>
2025-02-27 13:47:55 +0000

    High Resnet: Bigger 2D version

--- arch/unet/high_resnet.py
+++ arch/unet/high_resnet.py
@@ -2,6 +2,18 @@ from monai.networks.nets.highresnet import HighResNet as HighResnetModel
 
 from arch.unet.segmentation_base import SegmentationBase
 
+DEFAULT_LAYER_PARAMS_2D = (
+    # initial conv layer
+    {"name": "conv_0", "n_features": 64, "kernel_size": 3},
+    # residual blocks
+    {"name": "res_1", "n_features": 64, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_2", "n_features": 128, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_3", "n_features": 256, "kernels": (3, 3), "repeat": 3},
+    # final conv layers
+    {"name": "conv_1", "n_features": 512, "kernel_size": 1},
+    {"name": "conv_2", "kernel_size": 1},
+)
+
 class HighResnet(SegmentationBase):
     def __init__(
         self,
@@ -24,4 +36,5 @@ class HighResnet(SegmentationBase):
             in_channels=self.hparams.in_channels,
             out_channels=self.hparams.out_channels,
             spatial_dims=2,
+            layer_params=DEFAULT_LAYER_PARAMS_2D,
         )
